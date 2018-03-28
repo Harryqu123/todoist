42a1ab67c5be0a1db6d74f641754e7284f87fa92
Haoxuan Qu <haoxuanqu@gmail.com>
2018-03-28 06:19:41 +0000

    Finish static pages

---
+++ app/controllers/static_controller_controller.rb
@@ -0,0 +1,10 @@
+class StaticControllerController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def about 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_controller#home'
+  get 'static_controller/home'
+
+  get 'static_controller/help'
+  
+  get 'static_controller/about'
+  
+  get 'static_controller/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
